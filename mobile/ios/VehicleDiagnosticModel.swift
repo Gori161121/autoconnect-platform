2f08d03bb4eb49e8c735b862dc0bf969c7ffe203
Tural Alizada <115539521+Gori161121@users.noreply.github.com>
2026-06-18 19:46:42 +0200

    Add iOS diagnostic model

---
+++ mobile/ios/VehicleDiagnosticModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct VehicleDiagnosticModel {
+
+    let faultCode: String
+    let system: String
+    let severity: String
+    let description: String
+    let recommendedAction: String
+}
