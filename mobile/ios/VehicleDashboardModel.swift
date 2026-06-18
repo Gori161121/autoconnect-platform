01c2922c15aaa61d553098aca4d3117dd2821bb3
Tural Alizada <115539521+Gori161121@users.noreply.github.com>
2026-06-18 19:51:02 +0200

    Add iOS vehicle dashboard model

---
+++ mobile/ios/VehicleDashboardModel.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct VehicleDashboardModel {
+
+    let vehicleHealthScore: Int
+    let riskLevel: String
+
+    let driverScore: Int
+
+    let batteryHealth: Int
+
+    let insuranceRisk: String
+}
