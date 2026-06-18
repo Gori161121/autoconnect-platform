d53f2bf08d0ab7e8164c3f746e639bebd2daf188
Tural Alizada <115539521+Gori161121@users.noreply.github.com>
2026-06-18 19:49:58 +0200

    Add iOS driver behavior model

---
+++ mobile/ios/DriverBehaviorModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct DriverBehaviorModel {
+
+    let driverScore: Int
+    let behavior: String
+    let hardBrakes: Int
+    let rapidAccelerations: Int
+    let overspeedEvents: Int
+}
